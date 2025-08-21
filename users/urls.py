d4b9be7bd239a7d8c9fc3605d118cd02440d6727
Ahlal Aljasem <ahlalaljasem@gmail.com>
2025-08-21 09:34:38 +0300

    comment some urls

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import Get_All_Users,Get_All_Users_Sorted,ActivateAccount
 urlpatterns = [
     path('',include('djoser.urls')),
     path('',include('djoser.urls.jwt')),
-    path('All_Users/',Get_All_Users,name='Get_All_Users'), 
-    path('All_Users_Sort/',Get_All_Users_Sorted,name='Get_All_Users_Sorted'),
+    # path('All_Users/',Get_All_Users,name='Get_All_Users'), 
+    # path('All_Users_Sort/',Get_All_Users_Sorted,name='Get_All_Users_Sorted'),
     path('activate/<str:pk>/<str:token>/',ActivateAccount,name="ActivateAccount")   
 ]
